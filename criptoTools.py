47a85f1a3c45893382c67e24116bf3474d08b5a7
Verdi Esteban Rey Blanco <verdi07@localhost.localdomain>
2019-07-18 16:54:40 +0200

    Sent everything to recover branch

--- criptoTools.py
+++ criptoTools.py
@@ -11,8 +11,8 @@ def invMod(a, m):
     # number x which a * x mod x = 1
 
     if mcd(a, m) != 1:
-        return None
         # a and m are not coprimes, inverse module does not exist
+        return None
 
     # Euclides extended algorythm
     u1, u2, u3 = 1, 0, a
@@ -22,4 +22,3 @@ def invMod(a, m):
         q = u3 // v3
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
     return u1 % m
-
--- relatedFigure.py
+++ relatedFigure.py
@@ -1,7 +1,7 @@
 # Program that codes or decodes using the related figure method
 
 # Libraries
-import pyperclip, criptoTools, random
+import pyperclip as PC, criptoTools as CT, random
 
 # Declare the symbols availeable in our alphabet
 
@@ -9,24 +9,83 @@ SYMBOLS = """ !¡"#~½¬{}·$%&/()=?¿*ç^_:;,.-'ç+`ºª[\]1234567890qwertyuiop
 
 # The main function allows to select the coding or decoding mode
 def main():
-    mode = input('Select your mode: code a message(1) or decode a message(2): ')
+    mode = int(input('Select your mode: code a message(1), decode a message(2) or quit(0) but quitting is for loosers    AND    WE      AINT        LOSERS: \n'))
     while(mode != 1 or mode != 2 or mode != 0):
         if mode == 1:
-            msg = input('Enter the message you want to code')
-            a = int(input('Enter your decimation constant'))
-            b = int(input('Enter your displacement coefficient'))
-            text = code(text, a, b)
+            msg = input('Enter the message you want to code:\n')
+            a = int(input('Enter your decimation constant:\n'))
+            b = int(input('Enter your displacement coefficient:\n'))
+            text = code(msg, a, b)
+            PC.copy(text)
+            print('The coded text is: \n')
+            print(text)
             break
 
         elif mode == 2:
-            text = input('Enter the message you want to decode')
-            a = int(input('Enter the decimation constant')) 
-            b = int(input('Enter the displacement coefficient'))
+            text = input('Enter the message you want to decode:\n')
+            a = int(input('Enter the decimation constant:\n')) 
+            b = int(input('Enter the displacement coefficient:\n'))
             msg = decode(text, a, b)
+            PC.copy(msg)
+            print('The decoded message is: \n')
+            print(msg)
             break
 
         elif mode == 0:
             sys.exit()
 
         else:
-            print("""That was not and option, please, enter '1' to code a message, '2' to decode a message, or '0' to quit""")
+            print("""That was not and option, please, enter '1' to code a message, '2' to decode a message, or '0' to quit\n""")
+
+
+# Creates a random key (decimation constant and displacement coefficient)
+def genKey():
+    key = []
+    # Random integers can be between 2 (1 would make no difference and the amount of characters)
+    key.append(random.ranint(2, len(SYMBOLS)))
+    key.append(random.ranint(len(SYMBOLS)))
+    return key
+
+# Checks keys are OK
+def checkKeys(a, b):
+    if a not in range(2, len(SYMBOLS)):
+        return False
+    elif b not in range(len(SYMBOLS)):
+        return False
+    elif CT.mcd(a,b) != 1:
+        return False
+    else:
+        return True
+
+# Codes a message with the related figure algorythm
+def code(msg, a, b):
+    textToReturn = ''
+    if checkKeys(a, b):
+        for symbol in msg:
+            if symbol in SYMBOLS:
+                textToReturn += SYMBOLS[(SYMBOLS.index(symbol) * a + b) % len(SYMBOLS)]
+            else:
+                textToReturn += symbol
+        return textToReturn
+    else:
+        print('Please, enter compatible keys\n')
+        return None
+
+
+# Decodes a message with the related figure algorythm
+def decode(text, a, b):
+    msgToReturn = ''
+    if checkKeys(a, b):
+        invA = CT.invMod(a, len(SYMBOLS)) # Inverse module for the decode formula
+        for symbol in text:
+            if symbol in text:
+                msgToReturn += SYMBOLS[((SYMBOLS.index(symbol) - b) * invA) % len(SYMBOLS)]
+            else:
+                msgToReturn += symbol
+        return msgToReturn
+    else:
+        print('Please, enter compatible keys\n')
+        return None
+
+if __name__ == '__main__':
+    main()
